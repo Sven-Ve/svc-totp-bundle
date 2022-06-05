dfbe2db599e3b72faa60cd72dfad18003c3e80d0
Sven Vetter <git.09o@svenvetter.com>
2022-06-05 22:24:29 +0200

    commited by git_all.php on 2022-06-05 20:24:29

---
+++ translations/TotpBundle.de.php
@@ -0,0 +1,20 @@
+<?php
+
+return array (
+  'Manage 2FA' => '2FA verwalten',
+  'Use your Authenticator to Scan the QR Code' => 'Scanne mit deinem Authenticator den QR Code',
+  'Enable 2FA' => '2FA einschalten',
+  'Disable 2FA' => '2FA ausschalten',
+  'Two Factor Authentication' => 'Zweistufige Authentifizierung',
+  'Open your Authenticator app and type in the number' => 'Öffne deinen Authenticator und gib die Nummer ein',
+  'Clear trusted devices' => 'Vertrauenswürdige Geräte löschen',
+  'all' => 'alle',
+  '2FA is enabled.' => '2FA ist eingeschaltet.',
+  '2FA is disabled' => '2FA ist ausgeschaltet',
+  'If done, click on this button' => 'Wenn erledigt, dann klicke auf diese Schaltfläche',
+  'examples' => 'Beispiele',
+  'Please copy these backup codes to a safe place' => 'Bitte kopiere diese Backup Codes an eine sichere Stelle',
+  'If your authenticator is not available, you can use one of these codes (each code only once)' => 'Sollte dein Authenticator nicht verfügbar sein, kannst du einen dieser Codes benutzen (jeden Code nur einmal)',
+  'Backup codes' => 'Backup-Codes',
+  '' => '',
+);
